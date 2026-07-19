39e1b05f6a34271ab94bb574acade2ec3a5ce3de
venjiang <venjiang@gmail.com>
2026-07-19 20:35:34 +0800

    test: cover oversized server cert flight

--- src/quic/tls13.zig
+++ src/quic/tls13.zig
@@ -6917,7 +6917,7 @@ test "Tls13Handshake server rejects empty certificate entry without committing p
     try std.testing.expectEqual(HandshakeState.server_wait_client_hello, server.state);
 }
 
-test "Tls13Handshake server rejects oversized certificate flight before ServerHello" {
+test "Tls13Handshake server rejects oversized certificate flight without committing parsed state" {
     var hello_buf: [1024]u8 = undefined;
     const hello = try clientHelloBytes(.{}, &hello_buf);
     const oversized_cert = [_]u8{0xaa} ** 16372;
@@ -6927,9 +6927,26 @@ test "Tls13Handshake server rejects oversized certificate flight before ServerHe
         .private_key_bytes = &private_key,
         .private_key_algorithm = .ed25519,
     }, &[_]u8{});
+    server.peer_tp[0] = 0xde;
+    server.peer_tp[1] = 0xad;
+    server.peer_tp_len = 2;
+    server.peer_tp_available = true;
+    const old_peer_key_share = [_]u8{0x24} ** 32;
+    server.peer_x25519_public = old_peer_key_share;
+    const old_client_random = [_]u8{0x42} ** 32;
+    server.client_random = old_client_random;
+    server.client_random_available = true;
+    const transcript_before = server.transcript.current();
 
     server.provideData(hello);
     try std.testing.expectError(error.BadCertificate, server.step());
+    try std.testing.expectEqual(@as(usize, 0), server.out_len);
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0xde, 0xad }, server.peer_tp[0..server.peer_tp_len]);
+    try std.testing.expect(server.peer_tp_available);
+    try std.testing.expectEqualSlices(u8, &old_peer_key_share, &server.peer_x25519_public);
+    try std.testing.expectEqualSlices(u8, &old_client_random, &server.client_random);
+    try std.testing.expect(server.client_random_available);
+    try std.testing.expectEqualSlices(u8, &transcript_before, &server.transcript.current());
     try std.testing.expectEqual(HandshakeState.server_wait_client_hello, server.state);
 }
 
